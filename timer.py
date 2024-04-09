0c59eb4802c130961fd0dd763a4d7d2ce5feee89
Payal Kumavat <payalkumavat02@gmail.com>
2024-04-09 14:11:14 +0530

    Add user input prompts and countdown logic

--- timer.py
+++ timer.py
@@ -12,3 +12,14 @@ def individual_set_countdown(set_num, input_two, input_three):
         print("BREAK TIME: ", input_three, end='\r')
         time.sleep(1)
         input_three -= 1
+
+def jumrope_timer():
+    input_one = (int(input("Let's start! How many sets do you want to do today?: ")))
+    input_two = int(input("How long do you want each of your set to be? (in seconds): "))
+    input_three = int(input("Break in between the sets (in seconds): "))
+    for num in range(1, input_one + 1):
+        individual_set_countdown(num, input_two, input_three)
+    print(f"Congrats!!! All {input_one} sets completed")
+    exit()
+    
+jumrope_timer()
